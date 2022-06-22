97c9444afee5cef1498af259f979e625de8d5ce8
Sponkino <meetacmeinc@gmail.com>
2022-06-22 14:48:01 -0700

    exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-
-"""function prints elements of a list and handles the exceptions"""
-
-
 def safe_print_list(my_list=[], x=0):
-    index = 0
-    while True:
+    cmpt = 0
+    for index in range(x):
         try:
-            if index < x:
-                print(my_list[index], end='')
-                index += 1
-            else:
-                print()
-                return index
-        except IndexError:
-            print()
-            return 
+            print(my_list[index], end='')
+            cmpt += 1
+        except Exception as error:
+            break
+    print('')
+    return cmpt 
--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
-"""function that checks and prints integer and handles valueerror exception"""
-
-
 def safe_print_integer(value):
+
     try:
-        print("{:d}".format(value))
-        return True
-    except:
+        print('{:d}'.format(value))
+    except Exception as error:
         return False
+    else:
+        return True
